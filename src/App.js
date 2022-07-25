312a4945dfdd21c663f8334e187ed53b80a43cef
AidenOrt <aidenortiz1276@gmail.com>
2022-07-25 15:08:14 +0000

    fur

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
+import React, { z } from 'react';
 import PetNav from './components/pet-nav'
 import PictureBox from './components/picture-box'
 import './App.css';
+import AboutBox from './components/about-box';
 
 const App = () => {
   const component = new React.Component()
@@ -10,15 +11,68 @@ const App = () => {
     return(
       <div className="App">
         <PetNav/>
-        <div class="container">
-          <div class="row">
-            //Add profile picture and about section here
+        <div className="container">
+          
+            <PictureBox/>
+            <AboutBox/>
           </div>
-          <div class="row">
-            //Add posts here
-          </div>
+        <div className="row">
+
+        </div>
+        
+              
+        <div className="col-12">
+        <div className="list-group">
+          <a href="#" className="list-group-item list-group-item-action active">
+            <div>
+              <h5>Recent Posts</h5>
+            </div>
+          </a>
+          <a href="#" className="list-group-item list-group-item-action">
+            <div>
+              <h5>Takota McConner Says:</h5>
+              <small className="text-muted">3 days ago</small>
+            </div>
+            <p>Did nothing today, just layed around and ate food #bestdayever</p>
+            <input placeholder="comment.." />
+          </a>
+          <a href="#" className="list-group-item list-group-item-action">
+            <div>
+              <h5>Takota McConner Says:</h5>
+              <small className="text-muted">4 days ago</small>
+            </div>
+            <p>Didn't do much today, just layed around and ate food #yolo</p>
+            <input placeholder="comment.." />
+          </a>
+          <a href="#" className="list-group-item list-group-item-action">
+            <div>
+              <h5>Takota McConner Says:</h5>
+              <small className="text-muted">7 days ago</small>
+            </div>
+            <p>Nothing accomplished today, just layed around and ate food #livingmybestlife</p>
+            <input placeholder="comment.." />
+          </a>
+          <a href="#" className="list-group-item list-group-item-action">
+            <div>
+              <h5>Takota McConner Says:</h5>
+              <small className="text-muted">8 days ago</small>
+            </div>
+            <p>Slept all day and ate a few meals #killingthegame</p>
+            <input placeholder="comment.." />
+          </a>
+          <a href="#" className="list-group-item list-group-item-action">
+            <div>
+              <h5>Takota McConner Says:</h5>
+              <small className="text-muted">12 days ago</small>
+            </div>
+            <p>Was taken to the vet - considering puking in my owners shoes. Opinions?</p>
+            <input type="text" placeholder=" comment.." />
+          </a>
         </div>
       </div>
+    </div>
+          
+        
     )
   }
 
--- src/components/about-box.js
+++ src/components/about-box.js
@@ -1,4 +1,24 @@
-import React, { Component } from 'react'
+import React, { Compnent } from 'react';
 
+const AboutBox = () => {
+    return (
+      <div className='AboutBox'>
+      <div className="col-7 offset-1 about-box">
+    
+    <ul className="list-group">
+        <p className="list-group-item heading"><b>About</b></p>
+        <p className="list-group-item"><b>Age: 12</b></p>
+        <p className="list-group-item"><b>Occupation: Pet</b></p>
+        <p className="list-group-item"><b>Education: Wild Cat Academy</b></p>
+        <p className="list-group-item"><b>Favorite Bands: Cat Stevens, Def Leopard, Atomic Kitten, Lionel Ritchie</b></p>
+        <p className="list-group-item"><b>Contact Info: Meow at me at the fence behind Burger King on 242nd and Broadway</b></p>
+        <p className="list-group-item"><b>Relationship Status: Single</b></p>
+        <p className="list-group-item"><b>Places Lived: Paris, France and Bronx, New York</b></p>
+    </ul>
+    </div>
+    </div>
+ 
+    );
+}
 
-export default
+export default AboutBox;
